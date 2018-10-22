7165b609984c9c3ae2f546c0397e54a7c8dbaec4
Sungjae Hwang <sungjaehwang@Sungjaeui-MacBook-Pro.local>
2018-10-22 20:57:33 +0900

    regex_compile func branch coverage

--- extract.py
+++ extract.py
@@ -1,4 +1,5 @@
 import sys
+import re
 
 def main():
 	flag = False
@@ -6,7 +7,12 @@ def main():
 	st_total = 0.0
 	st_cover = 0.0
 	filename = sys.argv[1]
-	print "filename is " + filename
+	
+        regex_compile_total = 0.0
+        regex_compile_cover = 0.0
+        regex_compile_flag = False
+
+        print "filename is " + filename
 
 	f = open(filename, 'r')
 	while True:
@@ -14,18 +20,43 @@ def main():
 		if not line: break
 	
 		tmp = line.split(" ")
-		if (tmp[0] == "function") and (tmp[3] == "2"):
-			if flag == False:
-				branch_total = 0	
-				print "function name: " + tmp[1]
+		#if (tmp[0] == "function") and (tmp[3] == "2"):
+		if (tmp[0] == "function"):
+                        #print "in function:"
+                        if flag == False:
+				branch_total = 0
+                                if (tmp[1] == "regex_compile"):
+                                    regex_compile_flag = True
+				    print "function name: " + tmp[1]
+                                else: 
+                                    regex_compile_flag = False
+				    #print "function name: " + tmp[1]
+				
+                                #print "function name: " + tmp[1]
 				flag == True
 			
 			elif flag == True:
-				print "Total number of branches: " + str(branch_total)
-				branch_total = 0
-				print "function name: " + tmp[1]
+				#print "Total number of branches: " + str(branch_total)
+                                if (tmp[1] == "regex_compile"):
+                                    regex_compile_flag = True
+				    print "function name: " + tmp[1]
+                                else:
+                                    regex_compile_flag = False
+                                branch_total = 0
+				#print "function name: " + tmp[1]
 		elif (tmp[0] == "branch"):
-			branch_total += 1
+                       # print "in branch"
+                        if regex_compile_flag == True:
+                            regex_compile_total += 1
+                            if (line.find(' 0%') != -1):
+                                print "<regex_compile function: " + line + "not executed 0%>"
+                            elif line.find('never executed') != -1:
+                                print "<regex_compile function: " + line + "not executed never>"
+                            else:
+                                regex_compile_cover += 1
+                                print "<regex_compile function: " + line + "executed>"
+                            #print "HSJ : " + tmp[3]
+		        branch_total += 1
 		elif (tmp[4] == "#####:"):
                         st_total += 1
                         #print "####" + tmp[4]
@@ -37,7 +68,8 @@ def main():
 	
 	print "Total number of branches: " + str(branch_total)
 	print "Total number of statements: " + str(st_total)
-        print "Statement Coverage: " + str(st_cover) + "/" + str(st_total) + " = %.2f%%" % (st_cover/st_total * 100.0)
+        #print "Statement Coverage: " + str(st_cover) + "/" + str(st_total) + " = %.2f%%" % (st_cover/st_total * 100.0)
+        print "regex_compile func branch coverage: " + str(regex_compile_cover) + "/" + str(regex_compile_total) + " = %.2f%%" % (regex_compile_cover/regex_compile_total * 100.0)
 	f.close()
 
 if __name__ == '__main__':
